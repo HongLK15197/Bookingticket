8badb49c79f21246de29320cb876d69e87e60c69
Luu-Kim-Hong <kimhongluu1501@gmail.com>
2021-03-08 21:33:03 +0700

    routing_axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Contact from './pages/Contact/Contact';
+import Home from './pages/Home/Home';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
+  <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Header />
+
+      {/* Liên kết các page  */}
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/resgister" component={Register} />
+
+      {/* Route mặc định để dưới cuối cùng của ứng dụng */}
+        <Route exact path="/" component={Home}/>
+
+      {/* Switch và exact là lệnh để gọi đúng trang mà người dùng muốn đến (so sánh/break ra từng trang) */}
+      </Switch>
     </div>
+  </BrowserRouter>
   );
 }
 
---
+++ src/redux/reducer/configstore.js
@@ -0,0 +1,9 @@
+import {combineReducers, createStore} from 'redux';
+
+//state tổng cho ứng dựng
+
+const rootReducer = combineReducers({
+
+});
+
+export const store=createStore(rootReducer);
